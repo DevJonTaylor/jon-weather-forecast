7c8e874e6f2c71e6482159e7e3be7c1c1470cb41
Jon Taylor <31380713+DevJonTaylor@users.noreply.github.com>
2022-02-01 05:53:33 -0600

    Added search history and display Current and next five days.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,54 @@
-const geocode = new MapboxController('#geo-location');
-geocode.onResultsClear(onSelectedLocation);
+const WEATHER_API_KEY = '6acede01c67250672b90e28d885879dd';
+let searchContainer = new SearchHistory('#search-container');
+let offcanvasSearches = null;
+let forecastObj = new Forecast();
+
+document.onreadystatechange = function(event) {
+  if(document.readyState === 'complete') {
+    searchContainer.getLocalStorage();
+    offcanvasSearches = new bootstrap.Offcanvas(document.querySelector('#searches'));
+    offcanvasSearches.toggle();
+  }
+}
 
 function onSelectedLocation(event, selected) {
-  console.log(selected.name)
-  getWeatherData(selected.latitude, selected.longitude)
-    .then(json => console.log(json))
+  let search = new Search(selected.name, selected.latitude, selected.longitude);
+  searchContainer
+    .addSearch(search)
+    .render();
+
+  showWeather(search);
 }
 
-function addSearchHistory(selectedObject) {
-  let lat = selectedObject.latitude;
-  let lon = selectedObject.longitude;
-  let name = selectedObject.name;
-  document.querySelector('#geo-location')
-    .insertAdjacentHTML('beforeend',
-      `<button class='btn btn-dark' data-lat="${}">${selectedObject.name}</button>`)
+function onSearchAgain(event) {
+  let el = event.target;
+  if(el.dataset.id === undefined) return;
+
+  let search = searchContainer.searchById(el.dataset.id);
+  searchContainer.render();
+  showWeather(search);
 }
 
+function showWeather(search) {
+  let weatherOptions = new WeatherOptions(WEATHER_API_KEY);
+  weatherOptions
+    .lon(search.longitude)
+    .lat(search.latitude);
+
+  let weatherApi = new WeatherApi(weatherOptions);
+  
+  weatherApi.get()
+    .then(json => {
+      let daily = json.daily;
+      daily.pop();
+      
+      forecastObj.updateForecast(search.name, daily);
+    });
+
+  offcanvasSearches.toggle();
+}
+
+searchContainer.container.addEventListener('click', onSearchAgain)
+
+const geocode = new MapboxController('#geo-location');
+geocode.onResultsClear(onSelectedLocation);
